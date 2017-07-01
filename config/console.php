bba98fc10147392f8743c4d9275e40b422c72a60
Nikolay <n.poymanov@gmail.com>
2017-07-01 13:10:50 +0300

    Initial project

---
+++ config/console.php
@@ -0,0 +1,51 @@
+<?php
+
+function merge_configs($base, $customized)
+{
+    $baseConfig = require($base);
+    if (is_file($customized))
+        return yii\helpers\ArrayHelper::merge($baseConfig, require($customized));
+    return $baseConfig;
+}
+
+$params = merge_configs(__DIR__ . '/params.php', __DIR__ . '/params.local.php');
+$db = merge_configs(__DIR__ . '/db.php', __DIR__ . '/db.local.php');
+
+$config = [
+    'id' => 'basic-console',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'app\commands',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+    ],
+    'params' => $params,
+    /*
+    'controllerMap' => [
+        'fixture' => [ // Fixture generation command line.
+            'class' => 'yii\faker\FixtureController',
+        ],
+    ],
+    */
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
